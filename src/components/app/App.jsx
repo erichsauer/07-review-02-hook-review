4db5ba570d3139476971a54459213369ca2e393a
Erich Sauer <erich.sauer@gmail.com>
2021-06-01 11:55:07 -0700

    pretty much done

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+const useRecord = (init) => {
+  const [history, setHistory] = useState([init]);
+  const [current, setCurrent] = useState(init);
+  const record = (color) => {
+    setCurrent(color);
+    setHistory((prevHistory) => [...prevHistory, color]);
+  };
+
+  const undo = () => {
+    const i = history.indexOf(current);
+    setCurrent(history[i - 1]);
+  };
+
+  const redo = () => {
+    const i = history.indexOf(current);
+    setCurrent(history[i + 1]);
+  };
+
+  return { current, history, undo, redo, record };
+};
+
+function App() {
+  const { current, history, undo, redo, record } = useRecord('#FF0000');
+
+  return (
+    <>
+      <button onClick={undo} disabled={current === history[0]}>
+        undo
+      </button>
+      <button onClick={redo} disabled={current === history[history.length - 1]}>
+        redo
+      </button>
+      <input
+        type="color"
+        value={current}
+        onChange={({ target }) => record(target.value)}
+      />
+      <div
+        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}
+      ></div>
+    </>
+  );
 }
+
+export default App;
